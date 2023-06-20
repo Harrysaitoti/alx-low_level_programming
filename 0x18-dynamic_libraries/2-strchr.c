2e18eb0ee19bdfbf06954f310e682f00e6a18990
Harrysaitoti <saitotisunkuli@yahoo.com>
2023-06-20 07:12:32 -0700

    s

--- 0x18-dynamic_libraries/2-strchr.c
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -1,22 +1,22 @@
 #include "main.h"
+
 /**
- *@s: pointer block of memory to fill
- *@b: value to set
- *@n: bytes of the memory
- *Return: dest
+ * _memset - fills a block of memory with a specified value
+ * @s: pointer to the block of memory to fill
+ * @b: value to set
+ * @n: bytes of the memory
+ *
+ * Return: a pointer to the filled memory block
  */
-
 char *_memset(char *s, char b, unsigned int n)
-
 {
 	unsigned int i;
 
-	/*Delacring FOR*/
+	/* Delacring FOR */
 	for (i = 0; i < n; i++)
 	{
-		*(s + i) = b; /*add 1 position s*/
-
-	} /*END FOR*/
+		*(s + i) = b; /* add 1 position s */
+	} /* END FOR */
 
 	return (s);
 }
